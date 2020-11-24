e2d3abf64d8968458d865a37ec6b7730a2135026
Sethu Senthil <sethunarayansenthil@gmail.com>
2020-11-24 15:00:07 -0500

    Add Spinner Stuff

---
+++ SpinnerExample/app/src/main/java/com/example/spinnerexample/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.spinnerexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Spinner spinner;
+
+    ArrayList<String> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        spinner = findViewById(R.id.spinner);
+
+        list = new ArrayList<>();
+        list.add("Jim");
+        list.add("Bob");
+        list.add("Billy");
+        list.add("Jane");
+        list.add("Jen");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+}
---
+++ SpinnerPractice/app/src/main/java/com/example/spinnerpractice/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.spinnerpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Spinner spinner;
+    Button button;
+    ArrayList<String> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editTextTextPersonName);
+        spinner = findViewById(R.id.spinner);
+        button = findViewById(R.id.button);
+
+        list = new ArrayList<>();
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = editText.toString();
+                list.add(str);
+                arrayAdapter.notify();
+
+            }
+        });
+
+    }
+}
