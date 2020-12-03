72bac4b85987e3315ca6bf4a52f7ecbfe6eb18b9
Sethu Narayan Senthil <sethunarayansenthil@gmail.com>
2020-12-03 10:39:15 -0500

    Static Vars

---
+++ Static Variable/Data.java
@@ -0,0 +1,5 @@
+public class Data {
+    public final static String type = "BigData";
+    public final static int sets = 1;
+    public final static Boolean isFormatted = false;
+}
---
+++ Static Variable/Notes.java
@@ -0,0 +1,7 @@
+class Notes {
+    public static void main(String[] args) {
+       System.out.println(Data.type);
+       System.out.println(Data.sets);
+       System.out.println(Data.isFormatted);
+    }
+}
