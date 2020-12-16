e30f3c99e75d4a0a6baef1ed7bc6bbcd1f326f23
Sethu Narayan Senthil <sethunarayansenthil@gmail.com>
2020-12-15 19:49:50 -0500

    update project

--- ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ ListViewDemo/app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -31,12 +32,10 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.id_main_listview);
 
         arrayList = new ArrayList<>();
-        arrayList.add(new Karacter("Hello", "Hello", "Des"));
-        arrayList.add(new Karacter("Hello", "Hello", "Des"));
-        arrayList.add(new Karacter("Hello", "Hello", "Des"));
-        arrayList.add(new Karacter("Hello", "Hello", "Des"));
-        arrayList.add(new Karacter("Hello", "Hello", "Des"));
-        arrayList.add(new Karacter("Hello", "Hello", "Des"));
+        arrayList.add(new Karacter(R.drawable.anakin, "Anakin Skywalker", "Many know him as 'the chosen one'. Aspiring dark lord of the Sith!"));
+        arrayList.add(new Karacter(R.drawable.ahsoka, "Ahsoka Tano", "The Padawan of Anakin Skywalker who fought in the clonewars until she was falsely accused of treason and terrorism."));
+        arrayList.add(new Karacter(R.drawable.obiwan, "Obi-Wan Kenobi", "A nobel jedi who plays a big role in the skywalker saga, training both Anakin and his son. "));
+        arrayList.add(new Karacter(R.drawable.yoda, "Yoda", "Known for his unique way to articulate sentences backward, he is one of the most powerful and wisest jedis to exist during the fall of the republic"));
 
         listViewAdapter adapter = new listViewAdapter(this, R.layout.adapter_listview, arrayList);
         listView.setAdapter(adapter);
@@ -44,11 +43,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class Karacter {
-        public String url;
+        public int url;
         public String name;
         public String des;
 
-        public Karacter( String url, String name, String des){
+        public Karacter( int url, String name, String des){
             this.url = url;
             this.name = name;
             this.des = des;
@@ -74,8 +73,11 @@ public class MainActivity extends AppCompatActivity {
             LayoutInflater inflater = (LayoutInflater) mainActivityContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             View adapterLayout = inflater.inflate(xml, null);
             TextView textView = adapterLayout.findViewById(R.id.id_adapter_textview);
-
             textView.setText(list.get(position).name);
+
+            ImageView image = adapterLayout.findViewById(R.id.imageView);
+                    image.setImageResource(list.get(position).url);
+
             return adapterLayout;
 
         }
