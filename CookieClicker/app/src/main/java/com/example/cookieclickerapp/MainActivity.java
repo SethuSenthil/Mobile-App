aa0186d80507f1523371c3a386a94682e063542d
Sethu Narayan Senthil <sethunarayansenthil@gmail.com>
2021-03-04 13:59:36 -0500

    working async thread stuff

--- CookieClicker/app/src/main/java/com/example/cookieclickerapp/MainActivity.java
+++ CookieClicker/app/src/main/java/com/example/cookieclickerapp/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
@@ -15,18 +18,25 @@ import android.widget.TextView;
 
 import java.lang.reflect.Array;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
     ImageView cookie;
     ConstraintLayout layout;
     TextView counterView;
-    int counter;
+    int grogus;
     Button groguBtn;
+    AtomicInteger counter = new AtomicInteger(0);
     Context context = this;
+    String tag = "GROGUUU";
 
     public static float getRandom(float[] array) {
         int rnd = new Random().nextInt(array.length);
         return array[rnd];
+    }
+
+    public void syncText() {
+        counterView.setText("Cookies: " + counter.get());
     }
 
     @Override
@@ -38,10 +48,25 @@ public class MainActivity extends AppCompatActivity {
         counterView = findViewById(R.id.counterView);
         groguBtn = findViewById(R.id.groguBtn);
 
-
+        new AsyncThread().execute();
 
         ScaleAnimation scaleAnimation = new ScaleAnimation(1.0f, 1.15f, 1.0f, 1.15f, Animation.RELATIVE_TO_SELF, .5f, Animation.RELATIVE_TO_SELF, .5f);
 
+        groguBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter.getAndAdd(-20);
+                grogus++;
+
+                counterView.setText("Cookies: " + counter);
+
+                if(counter.get() >= 20){
+                    groguBtn.setEnabled(true);
+                }else{
+                    groguBtn.setEnabled(false);
+                }
+            }
+        });
 
 
         cookie.setOnClickListener(new View.OnClickListener() {
@@ -91,18 +116,84 @@ public class MainActivity extends AppCompatActivity {
 
                 constraintSet.setVerticalBias(textInCode.getId(), -0.5f);
 
+                //render Grogus
+
+                for (int i = 0; i < grogus; i++) {
+                    ImageView groguImg = new ImageView(context);
+                    groguImg.setId(View.generateViewId());
+                    groguImg.setImageResource(R.drawable.grogu);
+                    groguImg.getLayoutParams().height = 50;
+                    constraintSet.connect(groguImg.getId(), ConstraintSet.TOP, layout.getId(), ConstraintSet.TOP);
+                    constraintSet.connect(groguImg.getId(), ConstraintSet.BOTTOM, layout.getId(), ConstraintSet.BOTTOM);
+                    constraintSet.connect(groguImg.getId(), ConstraintSet.LEFT, layout.getId(), ConstraintSet.LEFT);
+                    constraintSet.connect(groguImg.getId(), ConstraintSet.RIGHT, layout.getId(), ConstraintSet.RIGHT);
+                    constraintSet.setVerticalBias(groguImg.getId(), -0.5f);
+                    constraintSet.setHorizontalBias(textInCode.getId(), 0.5f);
+                    groguImg.setVisibility(View.VISIBLE);
+                }
+
                 //random num for position
                float[] range = new float[]{0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f};
 
                 constraintSet.setHorizontalBias(textInCode.getId(), getRandom(range));
 
                 constraintSet.applyTo(layout);
-                counter++;
-                counterView.setText("Cookies: " + counter);
+                counter.getAndAdd(1);
+                counterView.setText("Cookies: " + counter.get());
+
+                if(counter.get() >= 20){
+                    groguBtn.setEnabled(true);
+                }else{
+                    groguBtn.setEnabled(false);
+                }
             }
         });
 
 
-
     }
+
+    public class AsyncThread extends AsyncTask<String, Void, Void> {
+
+        private final static int INTERVAL = 2000;
+        Handler mHandler = new Handler();
+
+        Runnable mHandlerTask = new Runnable()
+        {
+            @Override
+            public void run() {
+                counter.getAndIncrement();
+                syncText();
+                mHandler.postDelayed(mHandlerTask, INTERVAL);
+            }
+        };
+
+        void startRepeatingTask()
+        {
+            mHandlerTask.run();
+        }
+
+        void stopRepeatingTask()
+        {
+            mHandler.removeCallbacks(mHandlerTask);
+        }
+
+
+        @Override
+        protected Void doInBackground(String... voids) {
+            try {
+                startRepeatingTask();
+            } catch (Exception e) {
+                Log.d(tag, e.toString());
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
 }
